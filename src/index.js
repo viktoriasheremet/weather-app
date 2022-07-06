61818c44f18be16fad81b78443a2968e96bfabe2
viktoriasheremet <viktoriasheremet07@gmail.com>
2022-07-06 17:29:35 +0300

    showing the current temperature

---
+++ src/index.js
@@ -0,0 +1,40 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+document.querySelector("#date").innerHTML = `${day} ${hour}:${minutes}`;
+
+function displayTemperature(response) {
+  console.log(response.data.wind.speed);
+
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(
+    "#city"
+  ).innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "5e453ec37db04d7016a826b7caaff7a5";
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
